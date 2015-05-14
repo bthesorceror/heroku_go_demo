1b276995bf79e92c138bffa395e528dbcf7b55e9
bthesorceror <bthesorceror@gmail.com>
2015-05-13 19:18:43 -0600

    Render template for home page

--- main.go
+++ main.go
@@ -4,16 +4,14 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"html/template"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", hello).Methods("GET")
-	router.HandleFunc("/bye", bye).Methods("GET")
-	http.Handle("/", router)
+	http.Handle("/", createRouter())
 
 	fmt.Printf("Listening on port %s...\n", port)
 	err := http.ListenAndServe(":" + port, nil)
@@ -22,6 +20,23 @@ func main() {
 		panic(err)
 	}
 
+}
+
+func createRouter() (*mux.Router) {
+	files := http.FileServer(http.Dir("./assets/"))
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", index).Methods("GET")
+	router.HandleFunc("/hello", hello).Methods("GET")
+	router.HandleFunc("/bye", bye).Methods("GET")
+	router.PathPrefix("/").Handler(files)
+
+	return router
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	tmpl, _ := template.ParseFiles("./views/index.html")
+	tmpl.Execute(res, nil)
 }
 
 func bye(res http.ResponseWriter, req *http.Request) {
